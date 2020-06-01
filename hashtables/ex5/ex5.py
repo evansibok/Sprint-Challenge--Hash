04cf1ec508a7205499848cdda4a85d1c8189842c
Evans Ibok <evansibok@gmail.com>
2020-06-01 11:43:47 +0100

    Add query_table

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,10 +1,22 @@
 def finder(files, queries):
-
     """
     YOUR CODE HERE
     """
+    # Initialize a dictionary to hold key value pairs
+    # where keys are file path strings
+    # and values are the queries
+    query_table = {}
+    # Get the query to reconcile from the file path strings
 
-    return result
+    for file in files:
+        query = file.split('/')
+        query_table[file] = query[-1]
+
+    for query in queries:
+        print('query', query)
+
+    # return result
+    return query_table
 
 
 if __name__ == "__main__":
